482f90a320ccbc7712b96f9d55698cd94ea77dfd
Flan322 <arjun0kaushal@gmail.com>
2018-02-23 18:29:20 -0500

    Service Functionality
    Added in the basic example of service into the app. Need to now attach it to the graph for starting and stopping recording.

--- app/src/main/java/com/example/arjun/comfortmeter/MyService.java
+++ app/src/main/java/com/example/arjun/comfortmeter/MyService.java
@@ -3,18 +3,26 @@ package com.example.arjun.comfortmeter;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 public class MyService extends Service {
-
+    @Nullable
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        //TODO do something useful
-        return Service.START_NOT_STICKY;
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        //TODO for communication return IBinder implementation
-        return null;
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
     }
 }
